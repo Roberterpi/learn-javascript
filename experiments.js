42a865183636f111181ff0a506e878105d675b23
roberterpi <robertpopct@gmail.com>
2021-02-16 18:33:21 +0200

    wrote looping solution for looping exercise using for function

--- experiments.js
+++ experiments.js
@@ -3,6 +3,12 @@ const monthlyRent = 500;
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
 
+const character = 'f';
+const timesToRepeat = 5;
+let answer = '';
 
+for (let i = 0; i < timesToRepeat; i++) {
+  answer = answer + character;
+}
 
-
+console.log(answer);
