06cd06b32e0a456eb127b80f71ef13b96da45f3c
roberterpi <robertpopct@gmail.com>
2021-02-22 16:39:22 +0200

    solved quized

--- quiz.js
+++ quiz.js
@@ -24,47 +24,80 @@ function canada(userAnswer) {
   return isCorrect;
 }
 
-function uppercase(userAnswer, word) {
+function uppercase(userAnswer, home) {
   // what is <word> in all capital letters?
   // 200 pts
-  
+  const isCorrect = userAnswer === word.toUpperCase();
+
+  if (isCorrect) {
+    points += 200;
+  }
+  return isCorrect;
 }
 
-function firstThreeLetters(userAnswer, word) {
+function firstThreeLetters(userAnswer, home) {
   // what are the first three letters of <word>?
   // 200 pts
-  
+  const isCorrect = userAnswer === word.substr(o, 3);
+
+  if (isCorrect) {
+    points += 200;
+  }
+  return isCorrect;
 }
 
 function squared(userAnswer, number) {
   // what is <number> squared?
   // 200 pts
-  
   const isCorrect = userAnswer == number ** 2;
   
+  if (isCorrect) {
+    points += 200; 
+  }
+  return isCorrect;
 }
 
 function multiplication(userAnswer, num1, num2) {
   // what is <num1> multiplied by <num2>?
   // 300 pts
+  const isCorrect = userAnswer == num1 * num2;
   
+  if (isCorrect) {
+    points += 300; 
+  }
+  return isCorrect;
 }
 
 function age(userAnswer, currentYear, birthYear) {
   // if someone was born in <birthYear> and already has had their birthday this year, how old are they?
   // 300 pts
-
+  const isCorrect = userAnswer == currentYear - birthYear;
+  
+  if (isCorrect) {
+    points += 300; 
+  }
+  return isCorrect;
 }
 
 function larger(userAnswer, num1, num2) {
   // which of ${number4} and ${number5} is larger (if they're the same then pick that number)?
   // 300 pts
+  let isCorrect;
+  if (num1 > num2) {
+    isCorrect = userAnswer == num1
+  } else {
+    isCorrect = userAnswer == num2;
+  }
   
+  if (isCorrect) {
+    points += 300; 
+  }
+  return isCorrect;
 }
 
 function getScore() {
   // returns the current user's quiz score score
-
+  return points;
 }
 
 // DO NOT MODIFY CODE UNDER THIS COMMENT
